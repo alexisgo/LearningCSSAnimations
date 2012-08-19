f02217e5a71622acc482b221c65ed3272849381a
Alexis Goldstein <lex@livetotry.com>
2012-08-19 00:32:52 -0400

    adding ch3code

---
+++ ch3files/ch3code/box.js
@@ -0,0 +1,11 @@
+var box = document.getElementById("box");
+box.addEventListener("click", transition, false);
+
+function transition() {
+	if (this.className) {
+		this.className = "";		
+	}
+	else {
+		this.className = "translate";
+	}
+}
