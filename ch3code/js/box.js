38006e9991f6a7e45ffafd30f5b7332dbf8d1feb
alexisgo <lex@livetotry.com>
2012-08-20 23:50:47 -0400

    adding support for transition of color change on mobile (transition on click)

--- ch3code/js/box.js
+++ ch3code/js/box.js
@@ -2,10 +2,10 @@ var box = document.getElementById("box");
 box.addEventListener("click", transition, false);
 
 function transition() {
-	if (this.className) {
-		this.className = "";		
-	}
-	else {
-		this.className = "translate";
-	}
+  if (this.className) {
+    this.className = "";		
+  }
+  else {
+    this.className = "translate";
+  }
 }
