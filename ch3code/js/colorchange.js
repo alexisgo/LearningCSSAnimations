fb1ee61b855a27e626efb027d4349e34245d9d00
alexisgo <lex@livetotry.com>
2012-08-21 00:55:37 -0400

    rmeoved console logs

--- ch3code/js/colorchange.js
+++ ch3code/js/colorchange.js
@@ -2,12 +2,10 @@ var elem = document.getElementById("animateMe");
 elem.addEventListener("click", transition, false);
 
 function transition() {
-  console.log(this.className);
   if (this.className == "purple") {
     this.className = "blue";		
   }
   else if (this.className == "blue"){
     this.className = "purple";
   }
-  console.log("after "  +  this.className);
 }
