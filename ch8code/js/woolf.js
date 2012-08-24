cf36749c3d0b5c480be0c1d09540505aaea44d3b
alexisgo <lex@livetotry.com>
2012-08-24 04:36:51 -0400

    formatting changes

--- ch8code/js/woolf.js
+++ ch8code/js/woolf.js
@@ -26,31 +26,31 @@ function setup() {
 }
 
 function letters(initialDelay, selector, element, text) {
-/* include period, hyphen, and \w */
-var re = /[-.\w]/;     
-for (var i in text) {
-  if (text[i].match(re)) {
-    var el = document.createElement("span");
-    var letter = document.createTextNode(text[i]);
-    el.appendChild(letter);
-    $(element).append(el);
+  /* include period, hyphen, and \w */
+  var re = /[-.\w]/;     
+  for (var i in text) {
+    if (text[i].match(re)) {
+      var el = document.createElement("span");
+      var letter = document.createTextNode(text[i]);
+      el.appendChild(letter);
+      $(element).append(el);
+    }
+    else if (text[i] === "/") {
+      $(element).append(document.createElement("br"));
+    }
+    else {
+       $(element).append(document.createTextNode(text[i]));
+    }
   }
-  else if (text[i] === "/") {
-    $(element).append(document.createElement("br"));
-  }
-  else {
-     $(element).append(document.createTextNode(text[i]));
-  }
-}
-var delay = initialDelay;
-$(selector).each(function() {
-  if ($(this).is('span')) {
-    this.style[Modernizr.prefixed('animation')] = "letterAppear 100ms " + delay + "ms forwards";  
-    delay += 250;        
-  }
-  // If this is a br element, but NOT a br element with the class "skip"
-  else if ($(this).is('br') && ( ! $(this).is('.skip'))) {
-    delay += 1500;
-  } 
-});
+  var delay = initialDelay;
+  $(selector).each(function() {
+    if ($(this).is('span')) {
+      this.style[Modernizr.prefixed('animation')] = "letterAppear 100ms " + delay + "ms forwards";  
+      delay += 250;        
+    }
+    // If this is a br element, but NOT a br element with the class "skip"
+    else if ($(this).is('br') && ( ! $(this).is('.skip'))) {
+      delay += 1500;
+    } 
+  });
 } // end letters
